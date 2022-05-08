bb55dddabb7177979aee4ed95a5a9e48aed1a909
amendezm <alexis.david.mendez@gmail.com>
2022-05-07 21:12:44 -0400

    Initial timer animation

--- src/components/CountdownTimerItem.tsx
+++ src/components/CountdownTimerItem.tsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+
 const CountdownTimerItem = ({ counter, title }: IProps) => {
+  const [flip, setFlip] = useState(true);
+
+  useEffect(() => {
+    console.log("ENTER");
+
+    const interval = setInterval(() => {
+      console.log("test");
+      setFlip((flip) => !flip);
+      // setFlip((flip) => !flip);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="timer-item">
-      <div className="timer-item_square">{counter}</div>
-      <div className="timer-item_title">{title}</div>
+    // <div className="timer-item">
+    //   <div className="timer-item_square">{counter}</div>
+    //   <div className="timer-item_title">{title}</div>
+    // </div>
+    <div className={`flip-clock__piece ${flip ? "flip" : ""}`}>
+      <div className="flip-clock__card card">
+        <div className="card__top">{counter}</div>
+        <div className="card__bottom" data-value={counter}></div>
+        <div className="card__back" data-value={counter}>
+          <div className="card__bottom" data-value={counter}></div>
+        </div>
+      </div>
+      <div className="flip-clock__slot">{title}</div>
     </div>
   );
 };

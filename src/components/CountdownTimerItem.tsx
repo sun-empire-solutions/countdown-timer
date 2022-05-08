39b7df9da4baa19fc7f6366d052a6029342a1165
amendezm <alexis.david.mendez@gmail.com>
2022-05-08 15:05:27 -0400

    Set value after flip

--- src/components/CountdownTimerItem.tsx
+++ src/components/CountdownTimerItem.tsx
@@ -2,25 +2,29 @@ import { useEffect, useState } from "react";
 
 const CountdownTimerItem = ({ counter, title }: IProps) => {
   const [flip, setFlip] = useState(true);
+  const [value, setValue] = useState(null);
 
   useEffect(() => {
     setFlip((flip) => !flip);
+    setTimeout(() => {
+      setValue(counter);
+    }, 300);
   }, [counter]);
 
   return (
     <div className="timer-item">
       <div className="flipUnitContainer">
         <div className="upperCard">
-          <span>{counter}</span>
+          <span>{value ?? ""}</span>
         </div>
         <div className="lowerCard">
-          <span>{counter}</span>
+          <span>{value ?? ""}</span>
         </div>
         <div className={`flipCard ${flip ? "fold" : "unfold"}`}>
-          <span>{counter}</span>
+          <span>{value ?? ""}</span>
         </div>
         <div className={`flipCard ${flip ? "unfold" : "fold"}`}>
-          <span>{counter}</span>
+          <span>{value ?? ""}</span>
         </div>
       </div>
       <div className="timer-item_title">{title}</div>
